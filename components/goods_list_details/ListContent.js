8ebf12177ecabda4a5a06d7bdbf4b6576cc35ebd
xujingwei <1198513700@qq.com>
2021-01-19 22:05:58 +0800

    add:提交代码

---
+++ components/goods_list_details/ListContent.js
@@ -0,0 +1,26 @@
+// components/goods_list_details/ListContent.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    list:{
+      type:Array,
+      value:[]
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    test:'11'
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -10,13 +10,24 @@ Page({
     goods:[],//当前商品的数组
     queryKey:'',//模糊搜索关键字，
     curpage:1,//当前页
+
+    tabs:[{
+      id:1,
+      name:'综合'
+    },{
+      id:2,
+      name:'销量'
+    },{
+      id:3,
+      name:'价格'
+    }],//tab
+    activeTab:1,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    debugger
     let cid = options.cid
     this.setData({
       cid:cid
@@ -24,13 +35,18 @@ Page({
     this.getGoodsList()
   },
   getGoodsList:function (){
-    http({ url: '/goods/search',data:{query:this.data.queryKey,pagenum:this.data.curpage}}).then(res=>{
+    http({ url: '/goods/search',data:{query:this.data.queryKey,pagenum:this.data.curpage,pagesize:15}}).then(res=>{
       if(res&&res.data&&res.data.message){
         this.setData({
           goods:res.data.message.goods
         })
       }
     })
+  },
+  //tab点击事件
+  tabChange(tab){
+    let tabs = tab.detail
+    console.log(tabs)
   }
 
 })
