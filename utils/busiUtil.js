a8e019b8a539ff1328f7b7098ea58e34ed475d70
xujingwei <1198513700@qq.com>
2022-05-08 22:19:37 +0800

    增加新页面基础文件和业务工具函数

--- utils/busiUtil.js
+++ utils/busiUtil.js
@@ -5,7 +5,7 @@
  * @param {*} number 
  */
 const addBuyCars= (goods,number=1) => {
-  let buysList = wx.getStorage('buyCars')?  JSON.parse(wx.getStorage('buyCars')) : []
+  let buysList = wx.getStorageSync('buyCars')?  JSON.parse(wx.getStorageSync('buyCars')) : []
   let flag = 0 // flag 小于-1的时候，就代表已经找到了当前商品
   for(let i = 0;i<buysList.length ;i++) {
     let item = buysList[i]
@@ -18,9 +18,29 @@ const addBuyCars= (goods,number=1) => {
       }
     }
   }
-  wx.setStorage('buyCars', JSON.stringify(buysList))
+  wx.setStorageSync('buyCars', JSON.stringify(buysList))
 }
-
+const addSaveList= (goods,number=1) => {
+  let list = wx.getStorageSync('saveList')?JSON.parse(wx.getStorageSync('saveList')):[]
+  let flag = 0 // flag 小于-1的时候，就代表已经找到了当前商品
+  if(list.length > 0) {
+    for(let i = 0;i<list.length ;i++) {
+      let item = list[i]
+      if(item.goods.goodsId === goods.goodsId) {
+        item.number = item.number+number
+        break;
+      } else {
+        if(i===list.length-1) {
+          list.push({goods:goods,number: number})    
+        }
+      }
+    }
+  } else {
+    list.push({goods:goods,number: number})
+  }
+  wx.setStorageSync('saveList', JSON.stringify(list))
+}
 module.exports = {
-  addBuyCars: addBuyCars
+  addBuyCars: addBuyCars,
+  addSaveList: addSaveList
 }
